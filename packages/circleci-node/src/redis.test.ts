5eebf9924be1ba2dbd0a9eaa22fd9d9c5c678afb
chrisd08 <dalechristian@gmail.com>
2019-07-09 12:29:02 +0100

    fix CI

--- packages/circleci-node/src/redis.test.ts
+++ packages/circleci-node/src/redis.test.ts
@@ -1,4 +1,4 @@
-/* import * as Redis from "ioredis";
+import * as Redis from "ioredis";
 
 describe("redis tests", () => {
   it("ping", async () => {
@@ -7,4 +7,3 @@ describe("redis tests", () => {
     await redis.ping();
   });
 });
- */
