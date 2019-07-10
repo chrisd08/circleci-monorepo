3ced32b27faa0c8381c4e26a4461160e3865d255
chrisd08 <dalechristian@gmail.com>
2019-07-10 11:54:25 +0100

    test deploy

--- scripts/gather-dependencies.js
+++ scripts/gather-dependencies.js
@@ -3,6 +3,7 @@ exports.gatherDependencies = (info, workspace) => {
   let deps = [workspace];
   let ws = [workspace];
   while (ws.length) {
+    console.log(info, ws);
     info[ws[0]].workspaceDependencies.forEach(w => {
       ws.push(w);
       deps.push(w);
