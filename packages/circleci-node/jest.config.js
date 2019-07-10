91a05908cad7cc21ecdbe88895580a40c6f7d859
chrisd08 <dalechristian@gmail.com>
2019-07-10 23:27:34 +0100

    add test output to CI

--- packages/circleci-node/jest.config.js
+++ packages/circleci-node/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  reporters: ["default", "jest-junit"],
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testResultsProcessor: "jest-junit",
 };
